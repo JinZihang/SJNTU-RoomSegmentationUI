c0054052e81f2b2ae48e0de80ce6dca6f13b1b06
Jin Zihang <zihang.jin@gmail.com>
2021-09-18 23:45:27 +0800

    Complete line removal button function

--- src/app/room-seg-ui/room-seg-dialog/room-seg-dialog.component.ts
+++ src/app/room-seg-ui/room-seg-dialog/room-seg-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
     selector: 'room-seg-dialog',
@@ -9,11 +9,14 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 export class RoomSegDialog implements OnInit {
     title: string;
     content: string;
+    elementIndex: number;
     action: string;
+
 
     constructor(@Inject(MAT_DIALOG_DATA) data: any) {
         this.title = data.title;
         this.content = data.content;
+        this.elementIndex = data.elementIndex;
         this.action = data.action;
     }
 
--- src/app/room-seg-ui/room-seg-ui.component.ts
+++ src/app/room-seg-ui/room-seg-ui.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, Renderer2, Inject } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import linesetData from '../../assets/mock-lineset.json';
 import { RoomSegDialog } from './room-seg-dialog/room-seg-dialog.component'
@@ -11,17 +11,32 @@ const CanvasSideLength = 600;
   styleUrls: ['room-seg-ui.component.css']
 })
 export class RoomSegUIComponent implements AfterViewInit {
-  // Change to @Input after completing the UI
-  imageSrc = '/assets/mock-image.png';
-  linesets = linesetData.Linesets;
+  // Change imageSrc and linsets to @Input after completing the UI
+  // Add a @Output linesets variable after completing the UI
 
-  imageDim: number;
+  imageSrc: string = '/assets/mock-image.png';
+  imageDim: number; // imageDim > 1 if the image is vertical.
+
+  linesets: number[][] = linesetData.Linesets;
   extendedLinesets: number[][] = [];
+  linesetsBeforeEdit: number[][];
+  extendedLinesetsBeforeEdit: number[][];
+
+  lineSegment: boolean = true;
   extremities: number[] = [];
+  tempExtremEdit: boolean = false;
+
+  showCursor: boolean = false;
+  cursorPosition: number[] = [-1, -1];
+
+  processStart: boolean = false;
+  removalProcessStart: boolean = false;
 
   @ViewChild('dimContainer') dimContainerElement: ElementRef;
   @ViewChild('roomTopViewImage') imageElement: ElementRef;
+  @ViewChild('svg') svgElement: ElementRef;
   @ViewChild('line') lineElement: ElementRef;
+  @ViewChild('removeLineButton') removeLineButtonElement: ElementRef;
 
   constructor(private renderer: Renderer2, public dialog: MatDialog) {}
 
@@ -30,6 +45,8 @@ export class RoomSegUIComponent implements AfterViewInit {
   public roomImgOnLoad(): void {
     this.resizeDimContainer()
     this.extendLineSegments();
+
+    this.extendedLinesetsBeforeEdit = JSON.parse(JSON.stringify(this.extendedLinesets));
   }
 
   // Resize the dimContainerElement according to the original image dimension.
@@ -104,33 +121,96 @@ export class RoomSegUIComponent implements AfterViewInit {
     }
   }
 
-  public showExtremities(elementIndex: number): void {
-    this.extremities = elementIndex !== -1 ? this.linesets[elementIndex] : [];
+  public showExtremities(elementIndex: number, lineSegment: boolean): void {
+    let lineShrink = this.removalProcessStart ? 0 : 3;
+
+    if (lineSegment) {
+      this.extremities = elementIndex !== -1 ? this.linesets[elementIndex] : [];
+    } else if (this.removalProcessStart) {
+      this.extremities = elementIndex !== -1 ? this.linesets[elementIndex] : [];
+    } else if (elementIndex !== -1) {
+      this.tempExtremEdit = true;
+
+      this.extremities = this.extendedLinesets[elementIndex];
+      this.extremities[0] = this.extremities[0] === 0 ? lineShrink : this.extremities[0];
+      this.extremities[1] = this.extremities[1] === 0 ? lineShrink : this.extremities[1];
+
+      if (this.imageDim > 1) {
+        this.extremities[2] = this.extremities[2] === 600/this.imageDim ? 600/this.imageDim-lineShrink : this.extremities[2];
+        this.extremities[3] = this.extremities[3] === 600 ? 600-lineShrink : this.extremities[3];
+      } else {
+        this.extremities[2] = this.extremities[2] === 600 ? 600-lineShrink : this.extremities[2];
+        this.extremities[3] = this.extremities[3] === 600*this.imageDim ? 600*this.imageDim-lineShrink : this.extremities[3];
+      }
+    }
+
+    if (elementIndex === -1) {
+      this.extremities = [];
+      if (this.tempExtremEdit) {
+        this.extendedLinesets = JSON.parse(JSON.stringify(this.extendedLinesetsBeforeEdit));
+        this.tempExtremEdit = false;
+      }
+    }
   }
 
-  public moveSegLine(elementIndex: number): void {
-    this.openDialog('Edit this segmentation line?', 'Use cursor to move the line or key in its new extremities\' coordinates.', 'Edit');
+  public showCursorPosition(show: boolean): void {
+    this.showCursor = show;
   }
 
-  segAdd(): void {
-    this.openDialog('Add a segmentation line?', 'Use cursor to place extremities or key in their coordinates.', 'Add');
+  public updateCursorPosition(event: any): void {
+    this.cursorPosition[0] = event.clientX - 46;
+    this.cursorPosition[1] = event.clientY - 28;
   }
 
-  segRemove(): void {
-    // onclick change this button's color, then change the line onclick function to remove
+  public moveSegLine(elementIndex: number, confirmed: boolean): void {
+    if (confirmed) {
+      this.linesets = this.linesets.filter(e => e !== this.linesets[elementIndex]);
+      this.extendedLinesets = this.extendedLinesets.filter(e => e !== this.extendedLinesets[elementIndex]);
+      // Seg add a line
+    } else {
+      this.openDialog('Edit this segmentation line?', 'Use cursor to move the line or key in its new extremities\' coordinates.', elementIndex, 'Edit');
+    }
+  }
+
+  public lineSwitch(): void {
+    this.lineSegment = !this.lineSegment;
+  }
+
+  public segAdd(): void {
+    this.openDialog('Add a segmentation line?', 'Use cursor to place extremities or key in their coordinates.', this.linesets.length + 1, 'Add');
+  }
+
+  public segRemove(): void {
+    this.processStart = true;
+    this.removalProcessStart = true;
+
+    this.linesetsBeforeEdit = JSON.parse(JSON.stringify(this.linesets));
+    this.extendedLinesetsBeforeEdit = JSON.parse(JSON.stringify(this.extendedLinesets));
+  }
+
+  public removeLine(elementIndex: number): void {
+    this.linesets = this.linesets.filter(e => e !== this.linesets[elementIndex]);
+    this.extendedLinesets = this.extendedLinesets.filter(e => e !== this.extendedLinesets[elementIndex]);
+
+    this.extremities = [];
+
+    console.log('this.linesets');
+    console.log(this.linesets);
+    console.log('this.extendedLinesets');
+    console.log(this.extendedLinesets);
   }
   
-  segConfirm(): void {
-    this.openDialog('Proceed with this room segmentaion result?', 'Press cancel if you want to make further changes.', 'Proceed');
+  public segConfirm(): void {
+    this.openDialog('Proceed with this room segmentaion result?', 'Press cancel if you want to make further changes.', -1, 'Proceed');
   }
 
-  private openDialog(title: string, content: string, action: string): void {
+  private openDialog(title: string, content: string, elementIndex: number, action: string): void {
     const dialogConfig = new MatDialogConfig();
 
-    dialogConfig.disableClose = true;
     dialogConfig.data = {
       title: title,
       content: content,
+      elementIndex: elementIndex,
       action: action
     };
 
@@ -138,12 +218,13 @@ export class RoomSegUIComponent implements AfterViewInit {
 
     dialogRef.afterClosed().subscribe(result => {
       let confirm = result[0];
-      let action = result[1];
+      let elementIndex = result[1];
+      let action = result[2];
 
       if (confirm) {
         switch (action) {
           case 'Edit':
-            // Call the functions here.
+            this.moveSegLine(elementIndex, true)
             break;
           case 'Add':
             break;
@@ -155,5 +236,20 @@ export class RoomSegUIComponent implements AfterViewInit {
         }
       }
     });
+  }
+
+  public processControl(processConfirm: boolean): void {
+    if (!processConfirm) {
+      this.linesets = JSON.parse(JSON.stringify(this.linesetsBeforeEdit));
+      this.extendedLinesets = JSON.parse(JSON.stringify(this.extendedLinesetsBeforeEdit));
+    } else {
+      this.linesetsBeforeEdit = JSON.parse(JSON.stringify(this.linesets));
+      this.extendedLinesetsBeforeEdit = JSON.parse(JSON.stringify(this.extendedLinesets));
+    }
+
+    this.processStart = false;
+    if (this.removalProcessStart) {
+      this.removalProcessStart = false;
+    }
   }
 }
